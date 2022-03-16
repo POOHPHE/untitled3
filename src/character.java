97f91791e990d6537fbbcdc65f6e368de7d7ecb2
FALLENSLAYER007 <heartrockbreak@gmail.com>
2022-03-16 14:23:31 +0700

    template

---
+++ src/character.java
@@ -0,0 +1,8 @@
+
+public class character{
+    private int posX,posY;
+    private int hp;
+    private int range;
+    private int atkPoint;
+    private String symbol;
+}
---
+++ src/enemy.java
@@ -0,0 +1,5 @@
+public class enemy extends character{
+    public void Walk(){
+        //
+    }
+}
